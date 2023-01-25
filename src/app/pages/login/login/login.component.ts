e5d39b5149dc2a644fd31c41aec01dc32947174e
Igor marques <igormarquespe@gmail.com>
2023-01-25 17:57:53 -0300

    LoginComponent

---
+++ src/app/pages/login/login/login.component.ts
@@ -0,0 +1,28 @@
+import { SpotifyService } from './../../../services/spotify.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private spotifyService: SpotifyService) { }
+
+  ngOnInit(): void {
+    this.verificarTokenUrlCallback();
+  }
+
+  verificarTokenUrlCallback() {
+    const token = this.spotifyService.obterTokenUrlCallback();
+         if(!!token) {
+          this.spotifyService.definirAccessToken(token);
+        }    
+  }
+ 
+  abrirPaginaLogin() {
+     window.location.href = this.spotifyService.obterUrlLogin();
+    
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,21 @@ export const environment = {
   production: false
 };
 
+ export const SpotifyConfiguration = {
+   clientId: '52bf8ead241e428998e45c6d5f52dd84',
+   authEndpoint: 'https://accounts.spotify.com/authorize',
+   redirectUrl: 'http://localhost:4200/login/',
+   scopes: [
+     "user-read-currently-playing",
+     "user-read-recently-played",
+     "user-read-playback-state",
+     "user-top-read",
+     "user-modify-playback-state",
+     "user-library-read",
+     "playlist-read-private",
+     "playlist-read-collaborative"
+   ]
+ }
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
